5d5ee8353177bc25d666c5ae8123ceb690105352
unknown <shamiullipu@gmail.com>
2023-04-06 15:40:53 +0600

    children props cart and checkout page added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,6 +3,7 @@ import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fa
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
+import { Link } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -71,7 +72,11 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart} handleClearCart={handleClearCart}></Cart>
+                <Cart cart={cart} handleClearCart={handleClearCart}>
+                    <Link className='proceed-link' to='/orders'>
+                        <button className='btn-proceed'>Review Orders</button>
+                    </Link>
+                </Cart>
             </div>
         </div>
     );
